8eea2a137101fc95aeb60667a3e46ed7381e2f9b
Nikhil M S <nikhilms648@gmail.com>
2021-01-24 17:02:18 +0400

    test

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -11,15 +11,6 @@ export class CartService {
   selectedItemCount: any = {};
 
   constructor(private cookies: CookieService) {
-    if (this.cookies.check("user-info")) {
-      let userInfo = JSON.parse(this.cookies.get('user-info'));
-      if (this.cookies.check("cart-items-" + userInfo.username)) {
-        this.selectedItems = JSON.parse(this.cookies.get("cart-items-" + userInfo.username));
-      }
-      if (this.cookies.check("cart-items-count-" + userInfo.username)) {
-        this.selectedItemCount = JSON.parse(this.cookies.get("cart-items-" + userInfo.username));
-      }
-    }
   }
 
   addItem(item: any) {
@@ -29,7 +20,6 @@ export class CartService {
       this.selectedItemCount[item.id] = 1;
       this.selectedItems.push(item);
     }
-    this.updateCookie();
   }
 
   removeItem(item: any, index: number) {
@@ -39,29 +29,15 @@ export class CartService {
       delete this.selectedItemCount[item.id];
       this.selectedItems.splice(index, 1);
     }
-    this.updateCookie();
   }
 
   delete(item: any, index: number) {
     delete this.selectedItemCount[item.id];
     this.selectedItems.splice(index, 1);
-    this.updateCookie();
   }
 
   isSelected(item: any) {
     return this.selectedItemCount[item.id] != undefined;
   }
 
-  updateCookie() {
-    let userInfo = JSON.parse(this.cookies.get('user-info'));
-    if (this.cookies.check("cart-items-" + userInfo.username)) {
-      this.cookies.delete("cart-items-" + userInfo.username);
-    }
-    this.cookies.set('cart-items-' + userInfo.username, JSON.stringify(this.selectedItems), { expires: 2 });
-
-    if (this.cookies.check("cart-items-count-"+userInfo.username)) {
-      this.cookies.delete("cart-items-count-" + userInfo.username);
-    }
-    this.cookies.set('cart-items-count-' + userInfo.username, JSON.stringify(this.selectedItemCount), { expires: 2 });
-  }
 }
